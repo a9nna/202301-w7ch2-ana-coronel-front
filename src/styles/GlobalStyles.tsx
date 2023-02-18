757f9a0591bd607b6847bfc720ed7a0b0ca4e7c6
Marc Girbau <marc.girbau94@gmail.com>
2023-02-18 23:32:14 +0100

    Add button reset to GlobalStyles

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,14 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-*, 
-*:after,
-*:before {
-  margin: 0;
-  padding: 0;
+
+*, ::after, ::before {
   box-sizing: border-box;
-  text-decoration: none;
-  }
+}
 
 body {
   padding: 20px;
@@ -18,8 +14,20 @@ body {
   min-height: 100vh;
   
 }
+
 ol, ul, li {
   list-style: none;
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+
+button{
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-family: inherit;
+  
 }
 `;
 
