a045f1907506b854a7ae2ba27d0e685ea995c25a
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-18 13:52:20 +0100

    Add Layout styles & Global styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,11 @@
+import LayoutStyled from "./LayoutStyled";
+
 const Layout = (): JSX.Element => {
-  return <header className="main-header">Robots</header>;
+  return (
+    <LayoutStyled>
+      <header className="main-header">Robots</header>
+    </LayoutStyled>
+  );
 };
 
 export default Layout;
---
+++ src/components/Layout/LayoutStyled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const LayoutStyled = styled.div`
+  .main-header {
+    font-family: monospace;
+    font-weight: 800;
+    font-size: 20px;
+  }
+`;
+
+export default LayoutStyled;
