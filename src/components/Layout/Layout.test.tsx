cbd13873ab428ce2f14c5fe502567f4549bea49f
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-18 18:30:10 +0100

    Refactor Layout test component

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -1,12 +1,19 @@
 import Layout from "./Layout";
 import { render, screen } from "@testing-library/react";
+import Wrapper from "../../mocks/Wrapper";
+import GlobalStyles from "../../styles/GlobalStyles";
 
 describe("Given a Layout component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a title 'Robots'", () => {
       const titleText = "Robots";
 
-      render(<Layout />);
+      render(
+        <Wrapper>
+          <GlobalStyles />
+          <Layout />
+        </Wrapper>
+      );
 
       const expectedTitle = screen.getByRole("heading", { name: titleText });
 
