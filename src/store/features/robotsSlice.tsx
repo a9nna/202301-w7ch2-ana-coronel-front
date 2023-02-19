90a5b0fee2cdec9237ea914e2c815e6c07c17849
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-19 20:23:45 +0100

    Add createRobot action at robotsSlice

--- src/store/features/robotsSlice.tsx
+++ src/store/features/robotsSlice.tsx
@@ -12,11 +12,16 @@ const robotsSlice = createSlice({
     ],
     deleteRobot: (currentRobots, action: PayloadAction<RobotStructure>) =>
       currentRobots.filter((robot) => robot.id !== action.payload.id),
+    createRobot: (currentRobots, action: PayloadAction<RobotStructure>) => [
+      ...currentRobots,
+      action.payload,
+    ],
   },
 });
 
 export const {
   loadRobots: loadRobotsActionCreator,
   deleteRobot: deleteRobotActionCreator,
+  createRobot: createRobotActionCreator,
 } = robotsSlice.actions;
 export const robotsReducer = robotsSlice.reducer;
