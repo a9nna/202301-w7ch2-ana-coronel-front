d2310d911270030fb135de4347dd71ad23756afd
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-19 19:55:17 +0100

    Add styles to Form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,18 +2,38 @@ import FormStyled from "./FormStyled";
 
 const Form = (): JSX.Element => {
   return (
-    <FormStyled>
-      <label htmlFor="">Name</label>
-      <input type="text" />
-      <label htmlFor="">Image</label>
-      <input type="text" />
-      <label htmlFor="">Speed</label>
-      <input type="text" />
-      <label htmlFor="">Endurance</label>
-      <input type="text" />
-      <label htmlFor="">Creation date</label>
-      <input type="date" />
-      <button type="submit"></button>
+    <FormStyled className="form">
+      <label htmlFor="Name" aria-label="Robot name" className="form__name">
+        Name
+      </label>
+      <input type="text" placeholder="Name" className="form__input" />
+      <label htmlFor="Image" aria-label="Robot image" className="form__name">
+        Image
+      </label>
+      <input type="text" placeholder="Image" className="form__input" />
+      <label htmlFor="Speed" aria-label="Speed stat" className="form__name">
+        Speed
+      </label>
+      <input type="text" placeholder="Speed" className="form__input" />
+      <label
+        htmlFor="Endurance"
+        aria-label="Endurance stat"
+        className="form__name"
+      >
+        Endurance
+      </label>
+      <input type="text" placeholder="Endurance" className="form__input" />
+      <label
+        htmlFor="Creation date"
+        aria-label="Creation date"
+        className="form__name"
+      >
+        Creation date
+      </label>
+      <input type="date" className="form__input" />
+      <button type="submit" className="form__button">
+        Create Robot
+      </button>
     </FormStyled>
   );
 };
--- src/components/Form/FormStyled.tsx
+++ src/components/Form/FormStyled.tsx
@@ -3,6 +3,24 @@ import styled from "styled-components";
 const FormStyled = styled.form`
   display: flex;
   flex-direction: column;
+  gap: 10px;
+  padding: 30px;
+
+  .form {
+    &__input {
+      border: 1px solid gray;
+      border-radius: 10px;
+      padding: 0 6px;
+      font-size: 12px;
+      height: 30px;
+    }
+
+    &__button {
+      border-radius: 10px;
+      background-color: #006aff;
+      color: #fff;
+    }
+  }
 `;
 
 export default FormStyled;
