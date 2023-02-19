1175ba14f363ab20c462d227211dfd67d6b9508e
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-19 16:14:47 +0100

    Add RemoveButton component and RemoveButton functionality

---
+++ src/components/RemoveButton/RemoveButton.tsx
@@ -0,0 +1,16 @@
+import useApi from "../../hooks/useApi";
+
+import { RobotStructure } from "../../types/types";
+import Button from "../Button/Button";
+
+interface RemoveButtonProps {
+  robot: RobotStructure;
+}
+
+const RemoveButton = ({ robot }: RemoveButtonProps): JSX.Element => {
+  const { deleteRobots } = useApi();
+
+  return <Button icon="🗑️" actionOnClick={() => deleteRobots(robot)} />;
+};
+
+export default RemoveButton;
