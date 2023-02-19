c9caf9a75c71e2d86df593cb16b845cc579df04b
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-19 20:02:14 +0100

    Add testing to Form component

---
+++ src/components/Form/Form.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import Form from "./Form";
+
+describe("Given a Form component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show an label with an aria-label text 'Robot name'", () => {
+      const ariaLabelText = "Robot name";
+
+      render(<Form />);
+
+      const expectedText = screen.getByLabelText(ariaLabelText);
+
+      expect(expectedText).toBeInTheDocument();
+    });
+
+    test("Then it should show a button with the text 'Create Robot'", () => {
+      const buttonText = "Create Robot";
+
+      render(<Form />);
+
+      const expectedButtonText = screen.getByRole("button", {
+        name: buttonText,
+      });
+
+      expect(expectedButtonText).toBeInTheDocument();
+    });
+  });
+});
