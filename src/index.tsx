399a60037bbfcdad67add1e17a9a92db6616cc8e
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-19 11:06:01 +0100

    Add App testing

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import App from "./App";
+import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import GlobalStyles from "./styles/GlobalStyles";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
